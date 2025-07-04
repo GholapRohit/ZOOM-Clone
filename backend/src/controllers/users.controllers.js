752631d8f282e87a9393a388581de241376ae085
GholapRohit <rohitgholap2005@gmail.com>
2025-07-04 09:11:58 +0530

    Update files

--- backend/src/controllers/users.controllers.js
+++ backend/src/controllers/users.controllers.js
@@ -41,7 +41,7 @@ const login = async (req, res) => {
       res.cookie("token", jwtToken, {
         httpOnly: true,
         secure: true, // set false if testing on localhost without HTTPS
-        sameSite: "Strict",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000 * 3, // 3 days
       });
 
@@ -107,7 +107,7 @@ const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: true, // set false if testing on localhost without HTTPS
-      sameSite: "Strict",
+      sameSite: "none",
     });
 
     // Respond with success message
